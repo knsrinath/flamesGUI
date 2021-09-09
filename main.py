df698fcc0fcc7d31dae0fab8b7b8837b5b5ae7ff
knsrinath <knsrinath2005@gmail.com>
2021-09-09 17:07:53 +0530

    Update

--- main.py
+++ main.py
@@ -12,23 +12,15 @@ class MyWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="FLAMES")
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6, margin=8)
+        self.set_resizable(False)
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8, margin=10)
         self.add(vbox)
 
-        self.label1 = Gtk.Label()
-        self.label1.set_text("First Name")
-        self.label1.set_justify(Gtk.Justification.LEFT)
-        vbox.pack_start(self.label1, True, True, 0)
-
-        self.entry1 = Gtk.Entry()
+        self.entry1 = Gtk.Entry(placeholder_text="First Name")
         vbox.pack_start(self.entry1, True, True, 0)
 
-        self.label2 = Gtk.Label()
-        self.label2.set_text("Second Name")
-        self.label2.set_justify(Gtk.Justification.LEFT)
-        vbox.pack_start(self.label2, True, True, 0)
-
-        self.entry2 = Gtk.Entry()
+        self.entry2 = Gtk.Entry(placeholder_text="Second Name")
         vbox.pack_start(self.entry2, True, True, 0)
 
         self.button = Gtk.Button(label="Get FLAMES")
@@ -39,7 +31,18 @@ class MyWindow(Gtk.Window):
         firstName = self.entry1.get_text()
         secondName = self.entry2.get_text()
         res = flames(firstName, secondName)
-        os.system('notify-send ' + res)
+        if res == "Friends":
+            os.system('notify-send "Friends" "Maintain Friendship for ever"')
+        elif res == "Love":
+            os.system('notify-send "Love" ""')
+        elif res == "Attraction":
+            os.system('notify-send "Attraction" ""')
+        elif res == "Marriage":
+            os.system('notify-send "Marriage" ""')
+        elif res == "Enemy":
+            os.system('notify-send "Enemy" ""')
+        elif res == "Siblings":
+            os.system('notify-send "Siblings" ""')
 
 # Start GTK
 win = MyWindow()
