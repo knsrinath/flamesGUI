77f8dca257657bd1496aa0823ef2209d742ae10f
knsrinath <knsrinath2005@gmail.com>
2021-09-09 14:50:14 +0530

    add install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/knsrinath/flamesGUI flamesGUI
+mv flamesGUI /opt/
+echo 'python3 /opt/flamesGUI/main.py' >> /bin/flamesGUI
+echo '[Desktop Entry]
+Name=Flames GUI
+Comment=Get Flames For Two Names
+Keywords=flames;names
+Exec=flamesGUI
+Terminal=false
+Type=Application
+Icon=Terminal
+Categories=Games;
+StartupNotify=true' >> /usr/share/applications/flames.Desktop
